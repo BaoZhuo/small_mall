2c32411fe113dfca04b50fee927925286417c20c
baozhuo <291109232@qq.com>
2021-03-08 00:08:01 +0800

    first

---
+++ src/test/java/com/zhishi/demo/ZhishiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhishi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhishiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
